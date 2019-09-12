f20c2e8b36620db9a97145620da27c5cf86c0da3
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2019-09-12 22:55:33 +0530

    add image caching and static file cahing stratergies with old cache deletion logic

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,21 +1,27 @@
-const version = 'v1.0.0';
+const version = 'v1.0.2';
 // offline page url
 const offlinePage = 'offline.html';
-// html files cache
-const templateCache = `${version}templatefiles`;
+// static files cache
+const staticCache = `${version}staticfiles`;
 // image files cache
-const imageCache = `${version}imagefiles`;
-// stylesheets and js cache
-const miscCache = `${version}miscellaniousfiles`;
+const imageCache = `imagefiles`;
+
 // list of all caches
-const availableCaches = [templateCache,imageCache,miscCache];
+const availableCaches = [staticCache,imageCache];
 
 addEventListener('install',(ev)=>{
     ev.waitUntil(
-        caches.open(templateCache)
-        .then((htmlCache)=>{
-            return htmlCache.addAll([
-                offlinePage
+        caches.open(imageCache)
+        .then(imgCache =>{
+            imgCache.addAll([
+                '/favicon.png'
+            ]);
+        }),
+        caches.open(staticCache)
+        .then((statCache)=>{
+            return statCache.addAll([
+                offlinePage,
+                '/style.css'
             ]);
         })
         .catch((err)=>{
@@ -25,16 +31,55 @@ addEventListener('install',(ev)=>{
 });
 
 addEventListener('activate',(ev)=>{
-    console.log('service worker is activated...');
+    ev.waitUntil(
+        caches.keys().then(cacheNames =>{
+            return Promise.all(cacheNames.map(cache=>{
+                if(!availableCaches.includes(cache)){
+                    return caches.delete(cache);
+                }
+            }));
+        }).then(()=>{
+            return clients.claim();
+        })
+    );
 });
 
 addEventListener('fetch',(ev)=>{
     const request = ev.request;
+    // HTML file strategy
     if(request.mode === 'navigate' || request.headers.get('accept').includes('text/html')){
         ev.respondWith(
             fetch(request).catch(()=>{
                 return caches.match(offlinePage);
             })
         );
+        return;
     }
+    // Image file Strategy
+    if(request.headers.get('accept').includes('image')){
+        ev.respondWith(
+            caches.match(request).then((cachedImage)=>{
+                if(cachedImage){
+                    return cachedImage;
+                }
+                return fetch(request).then((response)=>{
+                    const imageresponse = response.clone();
+                    caches.open(imageCache).then(imgCache=>{
+                        imgCache.put(request, imageresponse);
+                    })
+                    return response;
+                })
+            })
+        );
+        return;
+    }
+    // other requests
+    ev.respondWith(
+        caches.match(request).then(cachedResponse => {
+            if(cachedResponse){
+                return cachedResponse;
+            }
+            return fetch(request);
+        })
+    );
 });
